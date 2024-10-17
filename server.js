2693faa259dd7ce41f09ffa7a96719674990429a
Dhamodharan-coder <dhamodharanmunus10@gmail.com>
2024-10-18 01:53:09 +0530

    add

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const student = require("./Users/users-content")
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://dhru-quizapp.netlify.app/", // Replace with the actual URL you want to allow
+        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+        credentials: true // Allow credentials (e.g., cookies)
+      }
+));
 
 connectDB(); // Connect to the database
 
