02d44a3dfab570bdb5d1bbcac59831a8bb9f98ef
Dhamodharan-coder <dhamodharanmunus10@gmail.com>
2024-10-18 02:05:25 +0530

    add

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: "https://dhru-quizapp.netlify.app/", // Replace with the actual URL you want to allow
+        origin: "https://dhru-quizapp.netlify.app", // Replace with the actual URL you want to allow
         methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
         credentials: true // Allow credentials (e.g., cookies)
       }
